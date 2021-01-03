c6d6fee84081a04104c3bd8c46a3bf38cd52762a
Dane Harrigan <72265290+dane@users.noreply.github.com>
2021-01-02 17:06:30 -0800

    Add service handlers and impl

---
+++ service/v1/impl.go
@@ -0,0 +1,43 @@
+package v1
+
+import (
+	"context"
+
+	pb "github.com/dane/skyfall/proto/gen/go/service/v1"
+)
+
+func (s *service) CreateAccountImpl(ctx context.Context, req *pb.CreateAccountRequest) (*pb.CreateAccountResponse, error) {
+	return &pb.CreateAccountResponse{}, nil
+}
+
+func (s *service) UpdateAccountImpl(ctx context.Context, req *pb.UpdateAccountRequest) (*pb.UpdateAccountResponse, error) {
+	return &pb.UpdateAccountResponse{}, nil
+}
+
+func (s *service) DeleteAccountImpl(ctx context.Context, req *pb.DeleteAccountRequest) (*pb.DeleteAccountResponse, error) {
+	return &pb.DeleteAccountResponse{}, nil
+}
+
+func (s *service) UndeleteAccountImpl(ctx context.Context, req *pb.UndeleteAccountRequest) (*pb.UndeleteAccountResponse, error) {
+	return &pb.UndeleteAccountResponse{}, nil
+}
+
+func (s *service) VerifyAccountImpl(ctx context.Context, req *pb.VerifyAccountRequest) (*pb.VerifyAccountResponse, error) {
+	return &pb.VerifyAccountResponse{}, nil
+}
+
+func (s *service) SuspendAccountImpl(ctx context.Context, req *pb.SuspendAccountRequest) (*pb.SuspendAccountResponse, error) {
+	return &pb.SuspendAccountResponse{}, nil
+}
+
+func (s *service) UnsuspendAccountImpl(ctx context.Context, req *pb.UnsuspendAccountRequest) (*pb.UnsuspendAccountResponse, error) {
+	return &pb.UnsuspendAccountResponse{}, nil
+}
+
+func (s *service) GetAccountImpl(ctx context.Context, req *pb.GetAccountRequest) (*pb.GetAccountResponse, error) {
+	return &pb.GetAccountResponse{}, nil
+}
+
+func (s *service) GetAccountByNameImpl(ctx context.Context, req *pb.GetAccountByNameRequest) (*pb.GetAccountByNameResponse, error) {
+	return &pb.GetAccountByNameResponse{}, nil
+}
---
+++ service/v1/service.go
@@ -0,0 +1,88 @@
+package v1
+
+import (
+	"context"
+
+	pb "github.com/dane/skyfall/proto/gen/go/service/v1"
+)
+
+type service struct {
+	pb.APIServiceServer
+	validator Validator
+}
+
+func New() (pb.APIServiceServer, error) {
+	return &service{}, nil
+}
+
+func (s *service) CreateAccount(ctx context.Context, req *pb.CreateAccountRequest) (*pb.CreateAccountResponse, error) {
+	if err := s.validator.CreateAccount(req); err != nil {
+		return nil, err
+	}
+
+	return s.CreateAccountImpl(ctx, req)
+}
+
+func (s *service) UpdateAccount(ctx context.Context, req *pb.UpdateAccountRequest) (*pb.UpdateAccountResponse, error) {
+	if err := s.validator.UpdateAccount(req); err != nil {
+		return nil, err
+	}
+
+	return s.UpdateAccountImpl(ctx, req)
+}
+
+func (s *service) DeleteAccount(ctx context.Context, req *pb.DeleteAccountRequest) (*pb.DeleteAccountResponse, error) {
+	if err := s.validator.DeleteAccount(req); err != nil {
+		return nil, err
+	}
+
+	return s.DeleteAccountImpl(ctx, req)
+}
+
+func (s *service) UndeleteAccount(ctx context.Context, req *pb.UndeleteAccountRequest) (*pb.UndeleteAccountResponse, error) {
+	if err := s.validator.UndeleteAccount(req); err != nil {
+		return nil, err
+	}
+
+	return s.UndeleteAccountImpl(ctx, req)
+}
+
+func (s *service) VerifyAccount(ctx context.Context, req *pb.VerifyAccountRequest) (*pb.VerifyAccountResponse, error) {
+	if err := s.validator.VerifyAccount(req); err != nil {
+		return nil, err
+	}
+
+	return s.VerifyAccountImpl(ctx, req)
+}
+
+func (s *service) SuspendAccount(ctx context.Context, req *pb.SuspendAccountRequest) (*pb.SuspendAccountResponse, error) {
+	if err := s.validator.SuspendAccount(req); err != nil {
+		return nil, err
+	}
+
+	return s.SuspendAccountImpl(ctx, req)
+}
+
+func (s *service) UnsuspendAccount(ctx context.Context, req *pb.UnsuspendAccountRequest) (*pb.UnsuspendAccountResponse, error) {
+	if err := s.validator.UnsuspendAccount(req); err != nil {
+		return nil, err
+	}
+
+	return s.UnsuspendAccountImpl(ctx, req)
+}
+
+func (s *service) GetAccount(ctx context.Context, req *pb.GetAccountRequest) (*pb.GetAccountResponse, error) {
+	if err := s.validator.GetAccount(req); err != nil {
+		return nil, err
+	}
+
+	return s.GetAccountImpl(ctx, req)
+}
+
+func (s *service) GetAccountByName(ctx context.Context, req *pb.GetAccountByNameRequest) (*pb.GetAccountByNameResponse, error) {
+	if err := s.validator.GetAccountByName(req); err != nil {
+		return nil, err
+	}
+
+	return s.GetAccountByNameImpl(ctx, req)
+}
